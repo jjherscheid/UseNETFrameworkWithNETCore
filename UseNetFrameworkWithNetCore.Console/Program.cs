2724e7ee1dd9b00f8702b6b2cfabaf835658e3e0
Johan Herscheid <johanherscheid@gmail.com>
2018-04-19 15:41:19 +0200

    Add project files.

---
+++ UseNetFrameworkWithNetCore.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UseNetFrameworkWithNetCore.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to this .NET Core Console App");
+
+
+            Console.ReadLine();
+        }
+    }
+}
